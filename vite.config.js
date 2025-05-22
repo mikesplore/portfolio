63d1e6b658ded5465e27ef1a605717f7111f02c2
mikesplore <mikepremium8@gmail.com>
2025-05-22 16:03:31 +0300

    updates

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // Replace 'portfolio' with your repository name
   server: {
     host: '0.0.0.0',   // Expose to local network
     port: 5173,        // Optional: choose your port
