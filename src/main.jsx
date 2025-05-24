6de2c5e32c9fb36668620c269da916e016df7746
mikesplore <mikepremium8@gmail.com>
2025-05-24 22:47:46 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// If you're using React Router, you would need to update it like:
+// import { BrowserRouter } from 'react-router-dom'
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <BrowserRouter basename="/portfolio">
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
